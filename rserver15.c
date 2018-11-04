37b1896cff702626a5526162844e041f4568eeb9
ARsquare <34487802+ARsquare@users.noreply.github.com>
2018-11-05 03:12:53 +0530

    Update rserver15.c

--- rserver15.c
+++ rserver15.c
@@ -1,62 +1,60 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-int main(void)
-{
-int listenfd = 0;
-int connfd = 0;
-struct sockaddr_in serv_addr;
-char sendBuff[1025];
-int numrv;
-int bytesReceived = 0;
-char recvBuff[256];
-memset(recvBuff, '0', sizeof(recvBuff));
-listenfd = socket(AF_INET, SOCK_STREAM, 0);
-printf("Socket retrieve success\n");memset(&serv_addr, '0', sizeof(serv_addr));
-memset(sendBuff, '0', sizeof(sendBuff));
-serv_addr.sin_family = AF_INET;
-serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-serv_addr.sin_port = htons(5000);
-bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
-if(listen(listenfd, 10) == -1)
-{
-printf("Failed to listen\n");
-return -1;
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+void main(){
+
+	int ssock,csock;
+	int n=0;
+	unsigned int len;
+	char s[100];
+	char c[1000];
+	FILE *f;
+
+	struct sockaddr_in server,client;
+
+	if((ssock=socket(AF_INET,SOCK_STREAM,0))==-1){
+		perror("socket: is not created");
+		exit(-1);
+	}
+
+	server.sin_family=AF_INET;
+	server.sin_port=htons(10000);
+	server.sin_addr.s_addr=INADDR_ANY;
+
+	bzero(&server.sin_zero,0);
+
+	len=sizeof(struct sockaddr_in);
+
+	if((bind(ssock,(struct sockaddr *)&server,len))==-1){
+		perror("bind: ");
+		exit(-1);
+	}
+
+	if((listen(ssock,5))==-1){
+		perror("listen: ");
+		exit(-1);
+	}
+
+
+	if((csock=accept(ssock,(struct sockaddr *)&client,&len))==-1){
+		perror("accept: ");
+		exit(-1);
+	}
+
+
+	recv(csock,&c,sizeof(c),0);
+	f=fopen("Copy.txt","wb");
+	fprintf(f,"%s\n",c);
+	fclose(f);
+
+	close(ssock);
+
 }
-while(1)
-{
-connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL);
-char file_name[20];
-recv(connfd,&file_name,sizeof(file_name),0);
-printf("file name : %s\n",file_name);
-/* Open the file that we wish to transfer */
-FILE *fp = fopen(file_name,"ab");
-if(fp==NULL)
-{
-printf("File opern error");
-return 1;
-}
-/* Receive data in chunks of 256 bytes */
-while((bytesReceived = read(connfd, recvBuff, 256)) > 0)
-{
-printf("Bytes received %d\n",bytesReceived);
-// recvBuff[n] = 0;
-fwrite(recvBuff, 1,bytesReceived,fp);
-// printf("%s \n", recvBuff);
-}
-if(bytesReceived < 0)
-{
-printf("\n Read Error \n");
-}
-fflush(fp);
-close(connfd);
-sleep(1);
-break;
-}
-return 0;}
