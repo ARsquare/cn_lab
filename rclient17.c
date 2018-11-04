7209d794a5a5e88d8bc2d41744af3af439de8a29
ARsquare <34487802+ARsquare@users.noreply.github.com>
2018-11-05 03:10:10 +0530

    Update rclient17.c

--- rclient17.c
+++ rclient17.c
@@ -8,34 +8,44 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include<string.h>
+
 void main(){
-int sock;
-unsigned int len;
-char s[100];
-char t[100]="Done";
-int i,n=0;
-struct sockaddr_in client;
-if((sock=socket(AF_INET,SOCK_STREAM,0))==-1){perror("socket: ");
-exit(-1);
+	int sock;
+	unsigned int len;
+	char s[100];
+	char t[100]="Done";
+	int i,n=0;
+
+	struct sockaddr_in client;
+	if((sock=socket(AF_INET,SOCK_STREAM,0))==-1){
+		perror("socket: ");
+		exit(-1);
+	}
+
+	client.sin_family=AF_INET;
+	client.sin_port=htons(10000);
+	client.sin_addr.s_addr=INADDR_ANY;
+
+	bzero(&client.sin_zero,0);
+
+
+	len=sizeof(struct sockaddr_in);
+	if((connect(sock,(struct sockaddr *)&client,len))==-1){
+		perror("connect: ");
+		exit(-1);
+	}
+
+	while(1){
+		printf("Enter order\n");
+		scanf("%s",s);
+		if(strcmp(s,t)==0)
+			break;
+		scanf("%d",&n);
+		send(sock,&s,sizeof(s),0);
+		send(sock,&n,sizeof(n),0);
+		recv(sock,&n,sizeof(n),0);
+		printf("Transction= %d\n",n);
+	}
+	close(sock);
+
 }
-client.sin_family=AF_INET;
-client.sin_port=htons(10000);
-client.sin_addr.s_addr=INADDR_ANY;
-bzero(&client.sin_zero,0);
-len=sizeof(struct sockaddr_in);
-if((connect(sock,(struct sockaddr *)&client,len))==-1){
-perror("connect: ");
-exit(-1);
-}
-while(1){
-printf("Enter order\n");
-scanf("%s",s);
-if(strcmp(s,t)==0)
-break;
-scanf("%d",&n);
-send(sock,&s,sizeof(s),0);
-send(sock,&n,sizeof(n),0);
-recv(sock,&n,sizeof(n),0);
-printf("Transction= %d\n",n);
-}
-close(sock);}
